c66792fec8f7f4f98117708d956337bee84f1e82
omarrendon <omarcr.96@gmail.com>
2020-05-26 13:20:10 -0500

    cambios a seccion de alumnos y definicion de asistencias

--- src/components/Asistencias.js
+++ src/components/Asistencias.js
@@ -48,34 +48,49 @@ export default class Asistencias extends Component {
 
   render() {
     return (
-      <table className="table table-striped">
-        <thead>
-          <tr>
-            <th scope="col"> ASIGNATURA</th>
-            <th scope="col"> NÚMERO DE ASISTENCIAS</th>
-            <th scope="col"> HORAS TOTALES</th>
-            <th scope="col"> FALTAS PERMITIDAS</th>
-            <th scope="col"> ALUMNO</th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.state.asistencias.map((asistencia) => (
-            <tr key={asistencia.id_alumo}>
-              <td>{asistencia.nombre2}</td>
-              <td>{asistencia.numero_asistencias}</td>
-              <td>{asistencia.horas}</td>
-              <td>{asistencia.faltas_permitidas}</td>
-              <td>
-                {asistencia.nombre} {asistencia.apellido_paterno}{" "}
-                {asistencia.apellido_materno}
-              </td>
-              {/* <td>
-                <button className="btn btn-success">Editar</button>
-              </td> */}
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <div className="container">
+      <div className="row">
+        <div className="col-md-12">
+          <div className="card card-body">
+            <p className="h3">Seleccionar Licenciatura</p>
+            <form onSubmit={this.handleSubmit}> 
+              <div className="form-gropup">
+                <select
+                  className="custom-select"
+                  name="carreras"
+                  // value={this.state.carreras}
+                  // onChange={this.onChange}
+                >
+                  {this.state.carreras.map((carrera) => (
+                    <option
+                      value={carrera.id_carrera}
+                      key={carrera.id_carrera}
+                    >
+                      {carrera.nombre}
+                    </option>
+                  ))}
+                </select>
+                <div className="dropdown-divider"></div>
+                <button type="submit" className="btn btn-success">
+                Guardar
+              </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      
+      <div className="dropdown-divider"></div>
+      
+      <div className="row">
+        <div className="col-md-12">
+          <div className="card card-body">
+            <p className="h3">Sleccionar Grupo</p>
+
+          </div>
+        </div>
+      </div>
+    </div>
     );
   }
 }
--- src/components/calificacion.js
+++ src/components/calificacion.js
@@ -102,153 +102,147 @@ export default class calificacion extends Component {
 
   render() {
     
-    return (
-      <div className="">
-        <div className="row">
-          <div className="col-md-12">
-            <div className="select">
-              <h3>Seleccionar Asignatura</h3>
-              <select
-                name="fk_materia"
-                className="custom-select custom-select-lg mb-3"
-              >
-                {this.state.materias.map(materia => (
-                  <option value={materia.id_materia} key={materia.id_materia}>
-                    {materia.nombre}
-                  </option>
-                ))}
-              </select>
-            </div>
-            <div className="dropdown-divider"></div>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-md-3">
-            <div className="card card-body">
-              <form onSubmit={this.calificar}>
-                <div className="form-group">
-                  <h3>Calificar Asignatura</h3>
-                  <input
-                    value={this.state.parciales.bimestre_uno}
-                    type="number"
-                    name="bimestre_uno"
-                    className="form-control"
-                    placeholder="Primer Bimestre"
-                    onChange={this.onChange}
-                  />
-                  <div className="dropdown-divider"></div>
-
-                  <input
-                    value={this.state.parciales.bimestre_dos}
-                    placeholder="Segundo Bimestre"
-                    type="number"
-                    name="bimestre_dos"
-                    className="form-control"
-                    onChange={this.onChange}
-                  />
-                  <div className="dropdown-divider"></div>
-
-                  <input
-                    value={this.state.parciales.ordinario}
-                    placeholder="Ordinario"
-                    type="number"
-                    name="ordinario"
-                    className="form-control"
-                    onChange={this.onChange}
-                  />
-                  <div className="dropdown-divider"></div>
-
-                  <input
-                    value={this.state.parciales.promedio_bimestral}
-                    placeholder="Promedio Bimestral"
-                    type="number"
-                    className="form-control"
-                    name="promedio_bimestral"
-                    onChange={this.onChange}
-                  />
-                  <div className="dropdown-divider"></div>
-
-                  <input
-                    value={this.state.parciales.promedio_final}
-                    placeholder="Promedio Final"
-                    type="number"
-                    name="promedio_final"
-                    placeholder-="Promedio Final"
-                    className="form-control"
-                    onChange={this.onChange}
-                  />
-                  <div className="dropdown-divider"></div>
-
-                  <input
-                    value={this.state.parciales.extraordinario}
-                    placeholder="Extraordinario"
-                    type="number"
-                    name="extraordinario"
-                    className="form-control"
-                    onChange={this.onChange}
-                  />
-                  <div className="dropdown-divider"></div>
-
-                  <input
-                    value={this.state.parciales.titulo}
-                    placeholder="Título"
-                    type="number"
-                    name="titulo"
-                    className="form-control"
-                    onChange={this.onChange}
-                  />
-                  <div className="dropdown-divider"></div>
-
-                  <input
-                    value={this.state.parciales.insuficiencia}
-                    placeholder="Insuficiencia"
-                    type="number"
-                    name="insuficiencia"
-                    className="form-control"
-                    onChange={this.onChange}
-                  />
-                  <div className="dropdown-divider"></div>
-                  <button type="submit" className="btn btn-success">
-                    Guardar
-                  </button>
-                </div>
-              </form>
-            </div>
-          </div>
-          <div className="col-md-9">
-            <table className="table">
-              <thead className="thead-dark">
-                <tr>
-                  <th scope="col"> Primer Bimestre</th>
-                  <th scope="col"> Segundo Bimestre</th>
-                  <th scope="col"> Ordinario</th>
-                  <th scope="col"> Promedio Bimestral</th>
-                  <th scope="col"> Promedio Final</th>
-                  <th scope="col"> Extraordinario</th>
-                  <th scope="col"> Título</th>
-                  <th scope="col"> Insuficiencia</th>
-                  {/* <th scope="col"> Editar</th> */}
-                </tr>
-              </thead>
-              <tbody>
-                {this.state.calificaciones.map(calificacion => (
-                  <tr key={calificacion.id_calificacion}>
-                    <th>{calificacion.bimestre_uno}</th>
-                    <th>{calificacion.bimestre_dos}</th>
-                    <th>{calificacion.ordinario}</th>
-                    <th>{calificacion.promedio_bimestral}</th>
-                    <th>{calificacion.promedio_final}</th>
-                    <th>{calificacion.extraordinario}</th>
-                    <th>{calificacion.titulo}</th>
-                    <th>{calificacion.insuficiencia}</th>
-                    {/* <th> <button className="btn btn-success"> Editar</button></th> */}
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </div>
-    );
+    return (  
+       <div className="">
+       <div className="row">
+         <div className="col-md-12">
+           <div className="select">
+             <h3>Seleccionar Asignatura</h3>
+             <select 
+               name="fk_materia"
+               className="custom-select custom-select-lg mb-3"
+             >
+               {this.state.materias.map(materia => (
+                 <option value={materia.id_materia} key={materia.id_materia}>
+                   {materia.nombre}
+                 </option>
+               ))}
+             </select>
+           </div>
+           <div className="dropdown-divider"></div>
+         </div>
+       </div>
+       <div className="row">
+         <div className="col-md-3">
+           <div className="card card-body">
+             <form onSubmit={this.calificar}>
+               <div className="form-group">
+                 <h3>Calificar Asignatura</h3>
+                 <input
+                   value={this.state.parciales.bimestre_uno}
+                   type="number"
+                   name="bimestre_uno"
+                   className="form-control"
+                   placeholder="Primer Bimestre"
+                   onChange={this.onChange}
+                 />
+                 <div className="dropdown-divider"></div>
+                 <input
+                   value={this.state.parciales.bimestre_dos}
+                   placeholder="Segundo Bimestre"
+                   type="number"
+                   name="bimestre_dos"
+                   className="form-control"
+                   onChange={this.onChange}
+                 />
+                 <div className="dropdown-divider"></div>
+                 <input
+                   value={this.state.parciales.ordinario}
+                   placeholder="Ordinario"
+                   type="number"
+                   name="ordinario"
+                   className="form-control"
+                   onChange={this.onChange}
+                 />
+                 <div className="dropdown-divider"></div>
+                 <input
+                   value={this.state.parciales.promedio_bimestral}
+                   placeholder="Promedio Bimestral"
+                   type="number"
+                   className="form-control"
+                   name="promedio_bimestral"
+                   onChange={this.onChange}
+                 />
+                 <div className="dropdown-divider"></div>
+                 <input
+                   value={this.state.parciales.promedio_final}
+                   placeholder="Promedio Final"
+                   type="number"
+                   name="promedio_final"
+                   placeholder-="Promedio Final"
+                   className="form-control"
+                   onChange={this.onChange}
+                 />
+                 <div className="dropdown-divider"></div>
+                 <input
+                   value={this.state.parciales.extraordinario}
+                   placeholder="Extraordinario"
+                   type="number"
+                   name="extraordinario"
+                   className="form-control"
+                   onChange={this.onChange}
+                 />
+                 <div className="dropdown-divider"></div>
+                 <input
+                   value={this.state.parciales.titulo}
+                   placeholder="Título"
+                   type="number"
+                   name="titulo"
+                   className="form-control"
+                   onChange={this.onChange}
+                 />
+                 <div className="dropdown-divider"></div>
+                 <input
+                   value={this.state.parciales.insuficiencia}
+                   placeholder="Insuficiencia"
+                   type="number"
+                   name="insuficiencia"
+                   className="form-control"
+                   onChange={this.onChange}
+                 />
+                 <div className="dropdown-divider"></div>
+                 <button type="submit" className="btn btn-success">
+                   Guardar
+                 </button>
+               </div>
+             </form>
+           </div>
+         </div>
+         <div className="col-md-9">
+           <table className="table">
+             <thead className="thead-dark">
+               <tr>
+                 <th scope="col"> Primer Bimestre</th>
+                 <th scope="col"> Segundo Bimestre</th>
+                 <th scope="col"> Ordinario</th>
+                 <th scope="col"> Promedio Bimestral</th>
+                 <th scope="col"> Promedio Final</th>
+                 <th scope="col"> Extraordinario</th>
+                 <th scope="col"> Título</th>
+                 <th scope="col"> Insuficiencia</th>
+                 {/* <th scope="col"> Editar</th> */}
+               </tr>
+             </thead>
+             <tbody>
+               {this.state.calificaciones.map(calificacion => (
+                 <tr key={calificacion.id_calificacion}>
+                   <th>{calificacion.bimestre_uno}</th>
+                   <th>{calificacion.bimestre_dos}</th>
+                   <th>{calificacion.ordinario}</th>
+                   <th>{calificacion.promedio_bimestral}</th>
+                   <th>{calificacion.promedio_final}</th>
+                   <th>{calificacion.extraordinario}</th>
+                   <th>{calificacion.titulo}</th>
+                   <th>{calificacion.insuficiencia}</th>
+                   {/* <th> <button className="btn btn-success"> Editar</button></th> */}
+                 </tr>
+               ))}
+             </tbody>
+           </table>
+         </div>
+       </div>
+     </div>  )
+     ;
   }
 }
+
