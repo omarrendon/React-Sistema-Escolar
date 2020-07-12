dc2cc3231268198584e03093f25efa4a2b011727
omarrendon <omarcr.96@gmail.com>
2020-07-11 23:20:56 -0500

    estilos modificados y terminados para laseccion de Asistencias

--- src/components/Asistencias.js
+++ src/components/Asistencias.js
@@ -1,25 +1,7 @@
 import React, { Component } from "react";
-// import React, { useState } from "react";
 import axios from "axios";
 import { Snackbar } from "./snackbar/Snackbar";
 import "./styles/Asistencias.css";
-
-// function Asistencias() {
-  
-//   const [appState, changeState]
-//   const [carreras, setCarreras] = useState([]);
-//   const [users, setUsers] = useState([]);
-//   const [asistencias, setAsistencias] = useState([]);
-//   const [carreraGrupos, setCarreraGrupos] = useState([]);
-//   const [materiaGrupo, setMateriaGrupo] = useState([]);
-//   const [alumnoGrupo, setAlumnoGrupo] = useState([]);
-//   const [asistenciaData, setAsistenciaData] = useState({
-//     numero_asistencias: "",
-//     fk_materia: "",
-//     fk_alumno: "",
-//   });
-
-// }
 
 export default class Asistencias extends Component {
   snackbarRef = React.createRef();
@@ -29,8 +11,16 @@ export default class Asistencias extends Component {
     colorState:{
       activeObj: null,
     },
-    carreraGrupos: [],
 
+    colorGrupo:{
+      activeObj: null,
+    },
+
+    colorMateria:{
+      activeObj:null
+    },
+
+    carreraGrupos: [],
     carreras: [],
     users: [],
     asistencias: [],
@@ -41,15 +31,12 @@ export default class Asistencias extends Component {
       numero_asistencias: "",
       fk_materia: "",
       fk_alumno: "",
-    },
-    color: 'red',
-    othercolor: 'blue'
+    }
   };
 
   componentDidMount() {
     this.getCarrera();
     this.getAsistencias();
-    this.getGrupoByCarrea()
   }
 
   getCarrera = async () => {
@@ -61,9 +48,8 @@ export default class Asistencias extends Component {
     console.log(this.state.carreras);
   };
 
-  getGrupoByCarrea = async (id_carrera, index) => {
+  getGrupoByCarrea = async (id_carrera) => {
     console.log(id_carrera);
-    console.log(index+1);
     
     const idCarrera = await axios.get(
       "http://localhost:8080/carreras/grupos?id_carrera=" + id_carrera
@@ -79,9 +65,7 @@ export default class Asistencias extends Component {
     console.log(this.state.carreraGrupos);
   };
   
-  changeStyle = (id_carrera, index) =>{
-    const newIndex = index+1;
-    console.log(newIndex);
+  changeStyle = (id_carrera) =>{
     console.log(this.state.carreraGrupos[id_carrera]);
     
     if(id_carrera === this.state.colorState.activeObj){
@@ -98,9 +82,22 @@ export default class Asistencias extends Component {
     );
     this.setState({
       materiaGrupo: materiasGrupo.data,
+      colorGrupo: {
+        ...this.state.colorGrupo, activeObj: id_grupo
+      }
     });
     console.log("MATERIAS_GRUPO");
     console.log(this.state.materiaGrupo);
+  };
+
+  changeColorGrupo = (id_grupo) => {
+    console.log(this.state.materiaGrupo[id_grupo]);
+    if(id_grupo === this.state.colorGrupo.activeObj){
+      return "active list-group-item"
+    }else {
+      return "inactive list-group-item"
+    }
+    
   };
 
   getAlumnosByMateria = async (id_materia) => {
@@ -110,9 +107,20 @@ export default class Asistencias extends Component {
     );
     this.setState({
       alumnoGrupo: alumnosGrupo.data,
+      colorMateria: {
+        ...this.state.colorMateria, activeObj: id_materia
+      }
     });
     console.log("ALUMNOS_GRUPO");
     console.log(this.state.alumnoGrupo);
+  };
+
+  changeColorMateria = (id_materia) => {
+    if(id_materia === this.state.colorMateria.activeObj){
+      return "active list-group-item"
+    }else {
+      return "inactive list-group-item"
+    }
   };
 
   getAsistencias = async () => {
@@ -175,10 +183,10 @@ export default class Asistencias extends Component {
 
     return (
       <div className="container p-4">
+        
         <p className="h2 text-center">Calificar Asistencias</p>
-
         <div className="row justify-content-md-center">
-
+          
           {/* SELECCIONAR LICENCIATURA */}
           <div className="col-12 col-md-4 col-sm-12">
             <div className="card card-body">
@@ -187,14 +195,9 @@ export default class Asistencias extends Component {
               <ul className=" list-group">
                 {this.state.carreras.map((carrera, index) => (
                   <li
-                    // className="list-group-item"
-                    className={this.changeStyle(carrera.id_carrera, index)}
-                    // style={() => this.getGrupoByCarrea(index)} 
+                    className={this.changeStyle(carrera.id_carrera)}
                     key={carrera.id_carrera}
-                    onClick={() => this.getGrupoByCarrea(carrera.id_carrera, index)}
-
-                    // style={ index+1 === carrera.id_carrera ? { background: this.state.color} : { background: this.state.othercolor }}
-
+                    onClick={() => this.getGrupoByCarrea(carrera.id_carrera)}
                   >
                     {carrera.nombre}
                   </li>
@@ -211,7 +214,7 @@ export default class Asistencias extends Component {
               <div className="container">
                 {this.state.carreraGrupos.map((id) => (
                   <div
-                    className="card card-body"
+                    className={this.changeColorGrupo(id.id_grupo)}
                     key={id.clave_grupo}
                     onClick={() => this.getMateriasByGruop(id.id_grupo)}
                   >
@@ -233,7 +236,7 @@ export default class Asistencias extends Component {
                 {this.state.materiaGrupo.map((materia) => (
                   <>
                     <div
-                      className="card card-body"
+                      className={this.changeColorMateria(materia.id_materia)}
                       key={materia.nombre}
                       onClick={() =>
                         this.getAlumnosByMateria(materia.id_materia)
